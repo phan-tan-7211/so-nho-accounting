45381c2708d67cfaaecea1bbe6dc69dc8bd9eaf0
phan-tan-7211 <116583632+phan-tan-7211@users.noreply.github.com>
2026-08-26 17:11:51 +0700

    test: remove app-scoped IndexedDB setup

--- src/test/setupIndexedDb.ts
+++
@@ -1,4 +0,0 @@
-if (process.env.CI === 'true' && typeof globalThis.indexedDB === 'undefined') {
-  const indexedDbHarness = 'fake-indexeddb/auto';
-  await import(/* @vite-ignore */ indexedDbHarness);
-}
