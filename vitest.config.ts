0e9a2b34edf7627ad2418b476ded4595b93505c3
phan-tan-7211 <116583632+phan-tan-7211@users.noreply.github.com>
2026-08-26 17:11:42 +0700

    test: point Vitest at external IndexedDB setup

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
   test: {
-    setupFiles: ['./src/test/setupIndexedDb.ts'],
+    setupFiles: ['./test/setupIndexedDb.ts'],
   },
 });
